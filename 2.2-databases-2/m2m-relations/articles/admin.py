e4aed434305b7580e9367e625d339e7d6d1b2332
ayanchevsky <minoz@ya.ru>
2022-11-27 14:52:44 +0400

    Доработка

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -5,17 +5,23 @@ from django.forms import BaseInlineFormSet
 from .models import Article, Tag, Scope
 
 
-# class RelationshipInlineFormset(BaseInlineFormSet):
-#     def clean(self):
-#         for form in self.forms:
-#             # В form.cleaned_data будет словарь с данными
-#             # каждой отдельной формы, которые вы можете проверить
-#             form.cleaned_data
-#             # вызовом исключения ValidationError можно указать админке о наличие ошибки
-#             # таким образом объект не будет сохранен,
-#             # а пользователю выведется соответствующее сообщение об ошибке
-#             raise ValidationError('Тут всегда ошибка')
-#         return super().clean()  # вызываем базовый код переопределяемого метода
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data and form.cleaned_data['is_main']:
+                count += 1
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            # form.cleaned_data
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+        if count > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        elif count == 0:
+            raise ValidationError('Укажите основной раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
 
 
 @admin.register(Tag)
@@ -25,7 +31,7 @@ class TagAdmin(admin.ModelAdmin):
 
 class ScopeInLine(admin.TabularInline):
     model = Scope
-    # formset = RelationshipInlineFormset
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
